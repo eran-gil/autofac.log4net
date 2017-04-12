179742dc146b45c64940011646c075e2e7491679
123 <123@123.com>
2017-04-12 11:32:38 +0300

    Added a caching mechanism in order to use it later in the type-mapper

---
+++ src/Autofac.log4net/Caching/DictionaryKeyValueCache.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Autofac.log4net.Caching
+{
+    public class DictionaryKeyValueCache<TKey, TValue> : IKeyValueCache<TKey, TValue>
+    {
+        private readonly IDictionary<TKey, TValue> _cacheDictionary;
+
+        public DictionaryKeyValueCache()
+        {
+            _cacheDictionary = new Dictionary<TKey, TValue>();
+        }
+
+        public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+        {
+            return _cacheDictionary.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable) _cacheDictionary).GetEnumerator();
+        }
+
+        public void Add(KeyValuePair<TKey, TValue> item)
+        {
+            _cacheDictionary.Add(item);
+        }
+
+        public void Clear()
+        {
+            _cacheDictionary.Clear();
+        }
+
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            return _cacheDictionary.Contains(item);
+        }
+
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            _cacheDictionary.CopyTo(array, arrayIndex);
+        }
+
+        public bool Remove(KeyValuePair<TKey, TValue> item)
+        {
+            return _cacheDictionary.Remove(item);
+        }
+
+        public int Count
+        {
+            get { return _cacheDictionary.Count; }
+        }
+
+        public bool IsReadOnly
+        {
+            get { return _cacheDictionary.IsReadOnly; }
+        }
+
+        public IEnumerable<TKey> Keys
+        {
+            get { return _cacheDictionary.Keys; }
+        }
+
+        public IEnumerable<TValue> Values
+        {
+            get { return _cacheDictionary.Values; }
+        }
+
+        public void AddEntry(TKey key, TValue value)
+        {
+            _cacheDictionary[key] = value;
+        }
+
+        public TValue GetEntryValue(TKey key)
+        {
+            if (_cacheDictionary.ContainsKey(key))
+            {
+                return _cacheDictionary[key];
+            }
+            return default(TValue);
+        }
+    }
+}
---
+++ src/Autofac.log4net/Caching/IKeyValueCache.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Autofac.log4net.Caching
+{
+    public interface IKeyValueCache<TKey, TValue> : ICollection<KeyValuePair<TKey, TValue>>
+    {
+        IEnumerable<TKey> Keys { get; }
+
+        IEnumerable<TValue> Values { get; }
+
+        void AddEntry(TKey key, TValue value);
+
+        TValue GetEntryValue(TKey key);
+    }
+}
