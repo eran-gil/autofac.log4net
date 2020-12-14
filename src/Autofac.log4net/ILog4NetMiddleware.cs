129d364c26cac979dd5424c639829831886203a1
Eran Gil <eran.gil2@gmail.com>
2020-12-14 19:30:37 +0200

    Made changes to match Autofac 6
    Changed the use of autofac events and created a middleware while maintaining a module for easier use

---
+++ src/Autofac.log4net/ILog4NetMiddleware.cs
@@ -0,0 +1,9 @@
+﻿using Autofac.Core.Resolving.Pipeline;
+
+namespace Autofac.log4net
+{
+    public interface ILog4NetMiddleware : IResolveMiddleware
+    {
+        void Initialize();
+    }
+}
--- src/Autofac.log4net/Log4NetModule.cs
+++ src/Autofac.log4net/Log4NetModule.cs
@@ -1,136 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
+﻿
 using Autofac.Core;
 using Autofac.Core.Registration;
-using Autofac.log4net.log4net;
-using Autofac.log4net.Mapping;
-using log4net;
 
 namespace Autofac.log4net
 {
-    /// <summary>
-    /// This module adds the ability to integrate the log4net framework with Autofac, together with some unique features.
-    /// </summary>
     public class Log4NetModule : Module
     {
-        private readonly ILog4NetAdapter _log4NetAdapter;
-        private readonly ILoggerMapper _loggerMapper;
-        private const BindingFlags RelevantProeprties = BindingFlags.Public | BindingFlags.Instance;
+        private readonly ILog4NetMiddleware _middleware;
 
-        /// <summary>
-        /// Name of the configuration file to be used for the logger definition.
-        /// Default value is null;
-        /// </summary>
-        public string ConfigFileName { get; set; }
-
-        /// <summary>
-        /// This flag indicates whether the log4net configuration file should be watched for changes.
-        /// Default value is true.
-        /// </summary>
-        public bool ShouldWatchConfiguration { get; set; }
-
-        /// <summary>
-        /// Default Constructor for the Log4NetModule.
-        /// Creates the module with the default implementations for its dependencies.
-        /// </summary>
-        public Log4NetModule() :
-            this(new Log4NetAdapter(), new CachedDictionaryLoggerMapper())
+        public Log4NetModule(string configFileName, bool shouldWatchConfiguration)
         {
+            _middleware = new Log4NetMiddleware(configFileName, shouldWatchConfiguration);
         }
 
-        /// <summary>
-        /// This constructor is mainly used for dependency injection in testing.
-        /// Should not be used unless necessary to override the default implementations.
-        /// </summary>
-        /// <param name="log4NetAdapter">Adapter to the log4net framework</param>
-        /// <param name="loggerMapper">Mapper for logger names in order to allow easier injection of loggers</param>
-        public Log4NetModule(ILog4NetAdapter log4NetAdapter, ILoggerMapper loggerMapper)
+        public Log4NetModule(ILog4NetMiddleware middleware)
         {
-            _log4NetAdapter = log4NetAdapter;
-            _loggerMapper = loggerMapper;
-            ConfigFileName = null;
-            ShouldWatchConfiguration = true;
+            _middleware = middleware;
         }
 
-        /// <summary>
-        /// Maps a type to a logger name, so when the ILog injection occurs, it will choose the correct logger.
-        /// </summary>
-        /// <param name="type">Type to be mapped</param>
-        /// <param name="loggerName">Logger name to be injected into the mapped type</param>
-        public void MapTypeToLoggerName(Type type, string loggerName)
-        {
-            _loggerMapper.MapTypeToLoggerName(type, loggerName);
-        }
-
-        /// <summary>
-        /// Maps a namespace to a logger name, so when the ILog injection occurs, it will choose the correct logger.
-        /// The module mapper will always return the most specific mapped-namespace's logger.
-        /// </summary>
-        /// <param name="namespace">Namespace to be mapped</param>
-        /// <param name="loggerName">Logger name to be injected into all types in the mapped namespace</param>
-        public void MapNamespaceToLoggerName(string @namespace, string loggerName)
-        {
-            _loggerMapper.MapNamespaceToLoggerName(@namespace, loggerName);
-        }
-
-        /// <inheritdoc />
         protected override void AttachToComponentRegistration(IComponentRegistryBuilder componentRegistry, IComponentRegistration registration)
         {
-            registration.Preparing += OnComponentPreparing;
-
-            registration.Activated += (sender, e) => InjectLoggerProperties(e.Instance);
+            registration.PipelineBuilding += (sender, pipeline) =>
+            {
+                pipeline.Use(_middleware);
+            };
+            base.AttachToComponentRegistration(componentRegistry, registration);
         }
 
-        /// <inheritdoc />
         protected override void Load(ContainerBuilder builder)
         {
-            _log4NetAdapter.ConfigureAndWatch(ConfigFileName, ShouldWatchConfiguration);
+            _middleware.Initialize();
             base.Load(builder);
-        }
-
-        private void InjectLoggerProperties(object instance)
-        {
-            var instanceType = instance.GetType();
-            var properties = GetILogProperties(instanceType);
-            var logger = GetLoggerFromType(instanceType);
-
-            foreach (var propToSet in properties)
-            {
-                propToSet.SetValue(instance, logger, null);
-            }
-        }
-
-        private void OnComponentPreparing(object sender, PreparingEventArgs e)
-        {
-            e.Parameters = e.Parameters.Union(
-                new[]
-                {
-                    new ResolvedParameter(
-                        (p, i) => p.ParameterType == typeof(ILog),
-                        (p, i) => GetLoggerFromType(p.Member.DeclaringType)
-                    ),
-                });
-        }
-
-        private ILog GetLoggerFromType(Type type)
-        {
-            var loggerName = _loggerMapper.GetLoggerName(type);
-            var logger = _log4NetAdapter.GetLogger(loggerName);
-            return logger;
-        }
-
-        private static IEnumerable<PropertyInfo> GetILogProperties(IReflect instanceType)
-        {
-            var instanceProperties = instanceType.GetProperties(RelevantProeprties);
-            var writableLogProperties = instanceProperties.Where(IsWritableLogProperty);
-            return writableLogProperties;
-        }
-
-        private static bool IsWritableLogProperty(PropertyInfo p)
-        {
-            return p.PropertyType == typeof(ILog) && p.CanWrite && p.GetIndexParameters().Length == 0;
         }
     }
 }
